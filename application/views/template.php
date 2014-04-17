0ec5b39fdfae98c69ee2405a83bff874945fa319
Matthew Kinbaum <mkinbaum@gmail.com>
2014-04-17 16:15:53 -0400

    Fixed viewport for mobile devices
    Included a meta tag in the template that prevents the user from zooming
    in. The content of the page has been reformatted so that scrolling is
    only vertical.

--- application/views/template.php
+++ application/views/template.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
     <title><?php echo isset($title) ? $title.' - '.lang('website_title') : lang('website_title'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content="Waterfall sharing platform" />
