6176f65833ad8133f88fc1d8fb94dda0a8e1620c
mkush <mmk4838@rit.edu>
2014-04-28 15:26:32 -0400

    update about.php to add our names to the team page

--- application/views/about.php
+++ application/views/about.php
@@ -1,8 +1,27 @@
-<h1 class="cover-heading"><?php echo lang('about_title'); ?></h1>
-    <p class="lead"><?php echo lang('about_explain'); ?></p>
+<div class="team">
+<h2 class="teamtitledes"><?php echo lang('about_team_title_des'); ?></h2>
+	
+	<p>Andrew Gucwa</p>
+	<p>Katharine Read</p>
+	<p>Tyler Stegall</p>
+    
+<h2 class="teamtitledev"><?php echo lang('about_team_title_dev'); ?></h2>
+   
+	<p>Christopher Blackman</p>
+	<p>Jan Dvorak</p>
+	<p>Matthew Kinbaum</p>
+	<p>Megan Kushner</p>
+	<p>Tyler Sposato</p>
+   
     <br />
+</div>
+
+<!--<h2 class="cover-heading"><?php echo lang('about_title'); ?></h2>-->
+ <p class="lead"><?php echo lang('about_explain'); ?></p>
+ <p class="lead"><?php echo lang('about_sundfall_explain'); ?></p>
+    <!-- <br />
 <h2 class="cover-heading"><?php echo lang('about_cascade_title'); ?></h2>
     <p class="lead"><?php echo lang('about_cascade_explain'); ?></p>
     <br />
 <h2 class="cover-heading"><?php echo lang('about_soundfall_title'); ?></h2>
-    <p class="lead"><?php echo lang('about_sundfall_explain'); ?></p>
+    <p class="lead"><?php echo lang('about_sundfall_explain'); ?></p>-->
